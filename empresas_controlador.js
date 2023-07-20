bd209ad0134713cc32bc7278e5b1eb0e3d0103a2
AdrianJMendez <esusjadrian@gmail.com>
2023-07-19 22:58:45 -0600

    Creacion de landign, y jsons de empresas

---
+++ empresas_controlador.js
@@ -0,0 +1,328 @@
+const Empresas = [
+    {
+        "categoria": "Ropa",
+        "nombre": "Broks Brothers",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 50,
+            "unidades_disponibles": 100,
+            "unidades_vendidas": 30,
+            "id_producto": "BB001",
+            "nombre_producto": "Camisa de vestir",
+            "descripcion": "Camisa de vestir para caballero"
+          },
+          {
+            "precio": 60,
+            "unidades_disponibles": 80,
+            "unidades_vendidas": 20,
+            "id_producto": "BB002",
+            "nombre_producto": "Pantalón",
+            "descripcion": "Pantalón formal para caballero"
+          },
+          {
+            "precio": 70,
+            "unidades_disponibles": 120,
+            "unidades_vendidas": 50,
+            "id_producto": "BB003",
+            "nombre_producto": "Vestido",
+            "descripcion": "Vestido elegante para dama"
+          }
+        ]
+      },
+
+      {
+        "categoria": "Ropa",
+        "nombre": "Ralph Lauren",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 80,
+            "unidades_disponibles": 80,
+            "unidades_vendidas": 50,
+            "id_producto": "RL001",
+            "nombre_producto": "Polo",
+            "descripcion": "Polo de algodón para hombre"
+          },
+          {
+            "precio": 90,
+            "unidades_disponibles": 60,
+            "unidades_vendidas": 40,
+            "id_producto": "RL002",
+            "nombre_producto": "Chamarra",
+            "descripcion": "Chamarra de cuero para hombre"
+          },
+          {
+            "precio": 100,
+            "unidades_disponibles": 100,
+            "unidades_vendidas": 30,
+            "id_producto": "RL003",
+            "nombre_producto": "Vestido",
+            "descripcion": "Vestido de noche para mujer"
+          }
+        ]
+      },
+      
+      {
+        "categoria": "Ropa",
+        "nombre": "Hugo Boss",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 120,
+            "unidades_disponibles": 60,
+            "unidades_vendidas": 20,
+            "id_producto": "HB001",
+            "nombre_producto": "Traje",
+            "descripcion": "Traje de negocios para hombre"
+          },
+          {
+            "precio": 130,
+            "unidades_disponibles": 40,
+            "unidades_vendidas": 15,
+            "id_producto": "HB002",
+            "nombre_producto": "Corbata",
+            "descripcion": "Corbata de seda para hombre"
+          },
+          {
+            "precio": 110,
+            "unidades_disponibles": 80,
+            "unidades_vendidas": 30,
+            "id_producto": "HB003",
+            "nombre_producto": "Blusa",
+            "descripcion": "Blusa elegante para mujer"
+          }
+        ]
+      },
+
+      {
+        "categoria": "Ropa",
+        "nombre": "J Crew",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 60,
+            "unidades_disponibles": 70,
+            "unidades_vendidas": 25,
+            "id_producto": "JC001",
+            "nombre_producto": "Saco",
+            "descripcion": "Saco de lana para hombre"
+          },
+          {
+            "precio": 50,
+            "unidades_disponibles": 90,
+            "unidades_vendidas": 40,
+            "id_producto": "JC002",
+            "nombre_producto": "Camisa",
+            "descripcion": "Camisa casual para hombre"
+          },
+          {
+            "precio": 70,
+            "unidades_disponibles": 50,
+            "unidades_vendidas": 10,
+            "id_producto": "JC003",
+            "nombre_producto": "Falda",
+            "descripcion": "Falda a la rodilla para mujer"
+          }
+        ]
+      },
+
+      {
+        "categoria": "Ropa",
+        "nombre": "Nike",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 90,
+            "unidades_disponibles": 100,
+            "unidades_vendidas": 60,
+            "id_producto": "NK001",
+            "nombre_producto": "Tenis",
+            "descripcion": "Tenis deportivos para hombre"
+          },
+          {
+            "precio": 80,
+            "unidades_disponibles": 80,
+            "unidades_vendidas": 35,
+            "id_producto": "NK002",
+            "nombre_producto": "Playera",
+            "descripcion": "Playera de manga corta para mujer"
+          },
+          {
+            "precio": 100,
+            "unidades_disponibles": 120,
+            "unidades_vendidas": 70,
+            "id_producto": "NK003",
+            "nombre_producto": "Shorts",
+            "descripcion": "Shorts deportivos para hombre"
+          }
+        ]
+      },
+      
+      {
+        "categoria": "Productos_Varios",
+        "nombre": "Urban Outfitters",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 15,
+            "unidades_disponibles": 200,
+            "unidades_vendidas": 100,
+            "id_producto": "UO001",
+            "nombre_producto": "Poster",
+            "descripcion": "Poster decorativo"
+          },
+          {
+            "precio": 10,
+            "unidades_disponibles": 150,
+            "unidades_vendidas": 70,
+            "id_producto": "UO002",
+            "nombre_producto": "Taza",
+            "descripcion": "Taza con diseño"
+          },
+          {
+            "precio": 5,
+            "unidades_disponibles": 300,
+            "unidades_vendidas": 150,
+            "id_producto": "UO003",
+            "nombre_producto": "Tarjetas",
+            "descripcion": "Tarjetas de felicitación"
+          }
+        ]
+      },
+
+      {
+        "categoria": "Productos_Varios",
+        "nombre": "Target",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 25,
+            "unidades_disponibles": 300,
+            "unidades_vendidas": 120,
+            "id_producto": "TG001",
+            "nombre_producto": "Mochila",
+            "descripcion": "Mochila escolar con estampados"
+          },
+          {
+            "precio": 15,
+            "unidades_disponibles": 500,
+            "unidades_vendidas": 200,
+            "id_producto": "TG002",
+            "nombre_producto": "Botella de agua",
+            "descripcion": "Botella reutilizable"
+          },
+          {
+            "precio": 20,
+            "unidades_disponibles": 200,
+            "unidades_vendidas": 100,
+            "id_producto": "TG003",
+            "nombre_producto": "Cojín",
+            "descripcion": "Cojín decorativo"
+          }
+        ]
+      },
+
+      {
+        "categoria": "Productos_Varios",
+        "nombre": "Staples",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 8,
+            "unidades_disponibles": 500,
+            "unidades_vendidas": 300,
+            "id_producto": "SP001",
+            "nombre_producto": "Libretas",
+            "descripcion": "Libretas con espiral"
+          },
+          {
+            "precio": 5,
+            "unidades_disponibles": 800,
+            "unidades_vendidas": 400,
+            "id_producto": "SP002",
+            "nombre_producto": "Bolígrafos",
+            "descripcion": "Bolígrafos de tinta gel"
+          },
+          {
+            "precio": 15,
+            "unidades_disponibles": 200,
+            "unidades_vendidas": 100,
+            "id_producto": "SP003",
+            "nombre_producto": "Impresora",
+            "descripcion": "Impresora multifuncional"
+          }
+        ]
+      },
+
+      {
+        "categoria": "Productos_Varios",
+        "nombre": "Office Depot",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 12,
+            "unidades_disponibles": 300,
+            "unidades_vendidas": 150,
+            "id_producto": "OD001",
+            "nombre_producto": "Agendas",
+            "descripcion": "Agendas con diseños"
+          },
+          {
+            "precio": 6,
+            "unidades_disponibles": 500,
+            "unidades_vendidas": 200,
+            "id_producto": "OD002",
+            "nombre_producto": "Lápices",
+            "descripcion": "Lápices de colores"
+          },
+          {
+            "precio": 25,
+            "unidades_disponibles": 100,
+            "unidades_vendidas": 50,
+            "id_producto": "OD003",
+            "nombre_producto": "Escritorio",
+            "descripcion": "Escritorio de madera"
+          }
+        ]
+      },
+      
+      {
+        "categoria": "Productos_Varios",
+        "nombre": "Walmart",
+        "imagen": "url_de_la_imagen",
+        "productos": [
+          {
+            "precio": 20,
+            "unidades_disponibles": 400,
+            "unidades_vendidas": 250,
+            "id_producto": "WM001",
+            "nombre_producto": "Pelotas",
+            "descripcion": "Pelotas de fútbol"
+          },
+          {
+            "precio": 30,
+            "unidades_disponibles": 150,
+            "unidades_vendidas": 80,
+            "id_producto": "WM002",
+            "nombre_producto": "Sartenes",
+            "descripcion": "Sartenes antiadherentes"
+          },
+          {
+            "precio": 40,
+            "unidades_disponibles": 200,
+            "unidades_vendidas": 100,
+            "id_producto": "WM003",
+            "nombre_producto": "Televisor",
+            "descripcion": "Televisor LED de 50 pulgadas"
+          }
+        ]
+      }
+      
+      
+      
+      
+
+      
+
+];
